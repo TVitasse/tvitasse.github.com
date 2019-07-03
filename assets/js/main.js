b91d278e59ef5d7a64ef17fd23dbc2a6c9875b7f
Your Name <tvitasse@ADN-FR-340P.adneomgroup.local>
2019-07-03 10:28:43 +0200

    BEAUTIFY

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,174 +5,174 @@
 */
 
 document.addEventListener('DOMContentLoaded', function() {
-		var city = "lyon"
-		document.getElementById("city").innerHTML = city.charAt(0).toUpperCase() + city.slice(1);
+  var city = "lyon"
+  document.getElementById("city").innerHTML = city.charAt(0).toUpperCase() + city.slice(1);
 
-    var request = new XMLHttpRequest();
-		request.onreadystatechange = function(){
-			if (this.readyState == XMLHttpRequest.DONE && this.status == 200){
-				var response = JSON.parse(this.responseText);
-				var condition_img = "<img id='condition_img' class='center' src='"+response.current_condition.icon_big+"' alt='"+response.current_condition.condition+"' />"
-				document.getElementById("condition").innerHTML = condition_img;
-				document.getElementById("temperature").innerHTML = response.current_condition.tmp + "°C";
-				document.getElementById("date").innerHTML = response.current_condition.date;
-				document.getElementById("time").innerHTML = response.current_condition.hour;
-			}
-		};
-		request.open("GET", "https://www.prevision-meteo.ch/services/json/".concat(city));
-		request.send();
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+      var response = JSON.parse(this.responseText);
+      var condition_img = "<img id='condition_img' class='center' src='" + response.current_condition.icon_big + "' alt='" + response.current_condition.condition + "' />"
+      document.getElementById("condition").innerHTML = condition_img;
+      document.getElementById("temperature").innerHTML = response.current_condition.tmp + "°C";
+      document.getElementById("date").innerHTML = response.current_condition.date;
+      document.getElementById("time").innerHTML = response.current_condition.hour;
+    }
+  };
+  request.open("GET", "https://www.prevision-meteo.ch/services/json/".concat(city));
+  request.send();
 }, false);
 
 (function($) {
 
-	var $window = $(window),
-		$body = $('body'),
-		$header = $('#header'),
-		$titleBar = null,
-		$nav = $('#nav'),
-		$wrapper = $('#wrapper');
+  var $window = $(window),
+    $body = $('body'),
+    $header = $('#header'),
+    $titleBar = null,
+    $nav = $('#nav'),
+    $wrapper = $('#wrapper');
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '1025px',  '1280px' ],
-			medium:   [ '737px',   '1024px' ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ null,      '480px'  ],
-		});
+  // Breakpoints.
+  breakpoints({
+    xlarge: ['1281px', '1680px'],
+    large: ['1025px', '1280px'],
+    medium: ['737px', '1024px'],
+    small: ['481px', '736px'],
+    xsmall: [null, '480px'],
+  });
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+  // Play initial animations on page load.
+  $window.on('load', function() {
+    window.setTimeout(function() {
+      $body.removeClass('is-preload');
+    }, 100);
+  });
 
-	// Tweaks/fixes.
+  // Tweaks/fixes.
 
-		// Polyfill: Object fit.
-			if (!browser.canUse('object-fit')) {
+  // Polyfill: Object fit.
+  if (!browser.canUse('object-fit')) {
 
-				$('.image[data-position]').each(function() {
+    $('.image[data-position]').each(function() {
 
-					var $this = $(this),
-						$img = $this.children('img');
+      var $this = $(this),
+        $img = $this.children('img');
 
-					// Apply img as background.
-						$this
-							.css('background-image', 'url("' + $img.attr('src') + '")')
-							.css('background-position', $this.data('position'))
-							.css('background-size', 'cover')
-							.css('background-repeat', 'no-repeat');
+      // Apply img as background.
+      $this
+        .css('background-image', 'url("' + $img.attr('src') + '")')
+        .css('background-position', $this.data('position'))
+        .css('background-size', 'cover')
+        .css('background-repeat', 'no-repeat');
 
-					// Hide img.
-						$img
-							.css('opacity', '0');
+      // Hide img.
+      $img
+        .css('opacity', '0');
 
-				});
+    });
 
-			}
+  }
 
-	// Header Panel.
+  // Header Panel.
 
-		// Nav.
-			var $nav_a = $nav.find('a');
+  // Nav.
+  var $nav_a = $nav.find('a');
 
-			$nav_a
-				.addClass('scrolly')
-				.on('click', function() {
+  $nav_a
+    .addClass('scrolly')
+    .on('click', function() {
 
-					var $this = $(this);
+      var $this = $(this);
 
-					// External link? Bail.
-						if ($this.attr('href').charAt(0) != '#')
-							return;
+      // External link? Bail.
+      if ($this.attr('href').charAt(0) != '#')
+        return;
 
-					// Deactivate all links.
-						$nav_a.removeClass('active');
+      // Deactivate all links.
+      $nav_a.removeClass('active');
 
-					// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-						$this
-							.addClass('active')
-							.addClass('active-locked');
+      // Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+      $this
+        .addClass('active')
+        .addClass('active-locked');
 
-				})
-				.each(function() {
+    })
+    .each(function() {
 
-					var	$this = $(this),
-						id = $this.attr('href'),
-						$section = $(id);
+      var $this = $(this),
+        id = $this.attr('href'),
+        $section = $(id);
 
-					// No section for this link? Bail.
-						if ($section.length < 1)
-							return;
+      // No section for this link? Bail.
+      if ($section.length < 1)
+        return;
 
-					// Scrollex.
-						$section.scrollex({
-							mode: 'middle',
-							top: '5vh',
-							bottom: '5vh',
-							initialize: function() {
+      // Scrollex.
+      $section.scrollex({
+        mode: 'middle',
+        top: '5vh',
+        bottom: '5vh',
+        initialize: function() {
 
-								// Deactivate section.
-									$section.addClass('inactive');
+          // Deactivate section.
+          $section.addClass('inactive');
 
-							},
-							enter: function() {
+        },
+        enter: function() {
 
-								// Activate section.
-									$section.removeClass('inactive');
+          // Activate section.
+          $section.removeClass('inactive');
 
-								// No locked links? Deactivate all links and activate this section's one.
-									if ($nav_a.filter('.active-locked').length == 0) {
+          // No locked links? Deactivate all links and activate this section's one.
+          if ($nav_a.filter('.active-locked').length == 0) {
 
-										$nav_a.removeClass('active');
-										$this.addClass('active');
+            $nav_a.removeClass('active');
+            $this.addClass('active');
 
-									}
+          }
 
-								// Otherwise, if this section's link is the one that's locked, unlock it.
-									else if ($this.hasClass('active-locked'))
-										$this.removeClass('active-locked');
+          // Otherwise, if this section's link is the one that's locked, unlock it.
+          else if ($this.hasClass('active-locked'))
+            $this.removeClass('active-locked');
 
-							}
-						});
+        }
+      });
 
-				});
+    });
 
-		// Title Bar.
-			$titleBar = $(
-				'<div id="titleBar">' +
-					'<a href="#header" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
-				'</div>'
-			)
-				.appendTo($body);
+  // Title Bar.
+  $titleBar = $(
+      '<div id="titleBar">' +
+      '<a href="#header" class="toggle"></a>' +
+      '<span class="title">' + $('#logo').html() + '</span>' +
+      '</div>'
+    )
+    .appendTo($body);
 
-		// Panel.
-			$header
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'right',
-					target: $body,
-					visibleClass: 'header-visible'
-				});
+  // Panel.
+  $header
+    .panel({
+      delay: 500,
+      hideOnClick: true,
+      hideOnSwipe: true,
+      resetScroll: true,
+      resetForms: true,
+      side: 'right',
+      target: $body,
+      visibleClass: 'header-visible'
+    });
 
-	// Scrolly.
-		$('.scrolly').scrolly({
-			speed: 1000,
-			offset: function() {
+  // Scrolly.
+  $('.scrolly').scrolly({
+    speed: 1000,
+    offset: function() {
 
-				if (breakpoints.active('<=medium'))
-					return $titleBar.height();
+      if (breakpoints.active('<=medium'))
+        return $titleBar.height();
 
-				return 0;
+      return 0;
 
-			}
-		});
+    }
+  });
 
 })(jQuery);
